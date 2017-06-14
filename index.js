fb62a1390a86661d4898405c45135dd8b439860c
Omar Jbara <omar.jbara2@gmail.com>
2017-06-14 12:21:08 -0400

    Renamed theme name

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const schemeIndex = {
   'predawn': require('./scheme/predawn.js'),
   'seti': require('./scheme/seti.js'),
   'solarized-dark': require('./scheme/solarized-dark.js'),
-  'solarized-linking': require('./scheme/solarized-linking.js'),
+  'linking-material': require('./scheme/linking-material.js'),
   'solarized-light': require('./scheme/solarized-light.js'),
   'tomorrow': require('./scheme/tomorrow.js'),
   'tomorrow-night': require('./scheme/tomorrow-night.js'),
---
+++ scheme/linking-material.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  colors: {
+    black: '#263238',
+    red: '#ff5353',
+    green: '#8bc34a',
+    yellow: '#ffc107',
+    blue: '#03a9f4',
+    magenta: '#ff5353',
+    cyan: '#08bdd4',
+    white: '#eceff1',
+    lightBlack: 'rgba(255, 255, 255, 0.54)',
+    lightRed: '#ff5353',
+    lightGreen: '#9ccc65',
+    lightYellow: '#ffa000',
+    lightBlue: '#81d4fa',
+    lightMagenta: '#ad1457',
+    lightCyan: '#00bcd4',
+    lightWhite: '#eceff1',
+  },
+
+  // Default
+  backgroundColor: '#263238',
+  foregroundColor: '#eceff1',
+  cursorColor: '#03a9f4',
+  borderColor: '#073642',
+
+  // Accent color
+  accentColor: '#268BD2',
+
+  // Other
+  tabTitleColor: 'rgba(255, 255, 255, 0.2)',
+  selectedTabTitleColor: '#DDD',
+};
